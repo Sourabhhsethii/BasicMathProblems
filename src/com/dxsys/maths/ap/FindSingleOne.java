93e9af999ba957045b5c61e23bf36893a1d4e8b1
Sourabh Sethi <sourabhhsethii@linux.com>
2021-09-17 22:36:51 -0400

    Added NoOf1Bits and Find Single one problem from BIT manipulation.

---
+++ src/com/dxsys/maths/ap/FindSingleOne.java
@@ -0,0 +1,88 @@
+package com.dxsys.maths.ap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Problem Description
+
+Given an array of integers A, every element appears twice except for one. Find that single one.
+
+NOTE: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+
+
+Problem Constraints
+2 <= |A| <= 2000000
+
+0 <= A[i] <= INTMAX
+
+
+
+Input Format
+First and only argument of input contains an integer array A.
+
+
+
+Output Format
+Return a single integer denoting the single element.
+
+
+
+Example Input
+Input 1:
+
+ A = [1, 2, 2, 3, 1]
+Input 2:
+5 1 2 2 3 1
+
+ A = [1, 2, 2]
+ 3 1 2 2
+
+
+Example Output
+Output 1:
+
+ 3
+Output 2:
+
+ 1
+
+
+Example Explanation
+Explanation 1:
+
+3 occurs once.
+Explanation 2:
+
+1 occurs once.
+ */
+public class FindSingleOne {
+
+    public int singleNumber(final List<Integer> A) {
+
+        int result =0;
+        for(int i=0;i<A.size();i++){
+            result = result ^ A.get(i);
+        }
+
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        FindSingleOne obj = new FindSingleOne();
+
+        System.out.println("Enter Number of elements of array");
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+        ArrayList<Integer> num = new ArrayList<>();
+        for (int i = 0; i < n; i++){
+            num.add(input.nextInt());
+        }
+        System.out.println(obj.singleNumber(num));
+
+    }
+}
---
+++ src/com/dxsys/maths/ap/NoOf1Bits.java
@@ -0,0 +1,50 @@
+package com.dxsys.maths.ap;
+
+import java.util.Scanner;
+
+/*
+Number of 1 Bits
+Problem Description
+
+Write a function that takes an integer and returns the number of 1 bits it has.
+
+Problem Constraints
+1 <= A <= 109
+
+Input Format
+First and only argument contains integer A
+
+Output Format
+Return an integer as the answer
+
+Example Input
+Input1:
+11
+
+Example Output
+Output1:
+3
+
+Example Explanation
+Explaination:
+11 is represented as 1011 in binary.
+ */
+public class NoOf1Bits {
+
+    public int numSetBits(int A) {
+        int count =0;
+        for (int i=A;i>0; i=i/2 ){
+            if(!(i%2==0)){
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String ss[]) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        NoOf1Bits obj = new NoOf1Bits();
+        System.out.println(obj.numSetBits(size));
+    }
+}
